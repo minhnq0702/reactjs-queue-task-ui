885aa84d0819c0cdc35b7cc4f590a3c843ed060b
MingNe <minhnq.0702@gmail.com>
2024-06-28 15:42:42 +0700

    format tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-call, no-undef
-const { nextui } = require("@nextui-org/react");
+const { nextui } = require('@nextui-org/react');
 
 export default {
-  content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [nextui()],
-}
-
+};
