4ec0c1763d67f756c346527c5168450784eb4b24
MingNe <minhnq.0702@gmail.com>
2024-07-03 15:04:01 +0700

    update sidebar item

--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -7,10 +7,10 @@ interface CSidebarItemProps {
 
 const CSidebarItem = ({ children }: PropsWithChildren<CSidebarItemProps>) => {
   return (
-    <li className="flex flex-col items-center pr-2 text-left">
+    <li className="flex flex-col pr-2 text-left py-1">
       <Link to="#" className="flex flex-1">
         <span id="sidebar-item-logo" className="px-3"></span>
-        <span id="sidebar-item-label" className="flex-1">
+        <span id="sidebar-item-label" className="flex-1 truncate">
           {children}
         </span>
       </Link>
