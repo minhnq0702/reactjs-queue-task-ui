929ec57abbbe26ef34f25b31c4ae0ea171bc9fd9
MingNe <minhnq.0702@gmail.com>
2024-07-03 14:02:23 +0700

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ const { nextui } = require('@nextui-org/react');
 export default {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1360px',
+      xl: '1920px',
+    },
+    extend: {
+      colors: {
+        app: '#0E1217',
+        // default: '#1A202C',
+        // primary: '#0070f3',
+        // secondary: '#ff0080',
+        // accent: '#00ff00',
+      },
+      with: {
+        'screen-desktop': '100vw',
+      },
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+        340: '85rem',
+      },
+    },
   },
   darkMode: 'class',
   plugins: [nextui()],
