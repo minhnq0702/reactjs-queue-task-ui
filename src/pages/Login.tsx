f2f093c62ec3c79e64ebac08b9caa56933f37589
MingNe <minhnq.0702@gmail.com>
2024-07-04 11:43:46 +0700

    update login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,52 @@
+import { Button, Card, Input } from '@nextui-org/react';
+import { useState } from 'react';
+import { Form } from 'react-router-dom';
+
 const PLogin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // TODO: Implement login logic
+    console.log('Login', email, password);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-      <form action="POST">
-        <input type="input" />
-      </form>
+    <div className="max-w-screen-md mx-5 md:mx-auto md:mt-10">
+      <h1 className="py-5">Login</h1>
+      <Card className="px-12 py-6 w-full md:w-144">
+        <div>
+          <Form method="POST">
+            <Input
+              label="Email / Username"
+              variant="underlined"
+              size="lg"
+              className="mt-2"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              label="Password"
+              variant="underlined"
+              size="lg"
+              className="mt-5"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              type="button"
+              size="lg"
+              color="primary"
+              fullWidth={true}
+              className="mt-8 mb-2"
+              onClick={handleLogin}
+            >
+              Login
+            </Button>
+          </Form>
+        </div>
+      </Card>
     </div>
   );
 };
