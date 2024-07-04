3c051d0c706aa0334b9b339ede507b391c9f770c
MingNe <minhnq.0702@gmail.com>
2024-07-04 23:02:32 +0700

    remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './main.css';
-import store from './models/store.ts';
+import { store } from './models/store.ts';
 import AppRouter from './routes/index.tsx';
 
 // 1. import `NextUIProvider` component
@@ -10,15 +9,16 @@ import { NextUIProvider } from '@nextui-org/react';
 import { Provider } from 'react-redux';
 import { Toaster } from 'sonner';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <NextUIProvider>
-      <Provider store={store}>
-        <Toaster position="top-right" richColors closeButton />
-        <main className="dark text-foreground bg-background w-screen h-screen">
-          <RouterProvider router={AppRouter} />
-        </main>
-      </Provider>
-    </NextUIProvider>
-  </React.StrictMode>,
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  // <React.StrictMode>
+  <NextUIProvider>
+    <Provider store={store}>
+      <Toaster position="top-right" richColors closeButton />
+      <main className="dark text-foreground bg-background w-screen h-screen">
+        <RouterProvider router={AppRouter} />
+      </main>
+    </Provider>
+  </NextUIProvider>,
+  // </React.StrictMode>,
 );
