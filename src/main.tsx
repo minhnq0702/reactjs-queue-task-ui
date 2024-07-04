f8789c356347114b3e9ffca05f3f826d34d4a721
MingNe <minhnq.0702@gmail.com>
2024-07-04 14:50:36 +0700

    setup store into app

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './main.css';
+import store from './models/store.ts';
 import AppRouter from './routes/index.tsx';
 
 // 1. import `NextUIProvider` component
 import { NextUIProvider } from '@nextui-org/react';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <NextUIProvider>
-      <main className="dark text-foreground bg-background w-screen h-screen">
-        <RouterProvider router={AppRouter} />
-      </main>
+      <Provider store={store}>
+        <main className="dark text-foreground bg-background w-screen h-screen">
+          <RouterProvider router={AppRouter} />
+        </main>
+      </Provider>
     </NextUIProvider>
   </React.StrictMode>,
 );
