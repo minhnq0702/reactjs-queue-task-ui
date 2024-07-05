a07add1630d51ab20ba11873ebad2f41e6f4ac03
MingNe <minhnq.0702@gmail.com>
2024-07-05 14:16:39 +0700

    change file anme

--- src/pages/PTasks.tsx
+++ src/pages/PTasks.tsx
@@ -1,11 +1,12 @@
 import CSideModal from '@/components/shared/SideModal';
+import CTaskItem from '@/components/shared/TaskItem';
 import { actions } from '@/models/slices/SliceTask';
-import { useAppDispath } from '@/models/store';
+import { useAppDispath, useAppSelector } from '@/models/store';
 import { Button, useDisclosure } from '@nextui-org/react';
 import { useEffect } from 'react';
 
 const PTask = () => {
-  // const tasks = useAppSelector((state) => state.task.tasks);
+  const tasks = useAppSelector((state) => state.task.tasks);
   const dispath = useAppDispath();
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
@@ -21,6 +22,19 @@ const PTask = () => {
       <div>
         <h1>PTask</h1>
         <Button onClick={onOpen}></Button>
+        <div>
+          {tasks.map((task) => (
+            <CTaskItem
+              key={task.id}
+              id={task.id}
+              state={task.state}
+              model={task.model}
+              func={task.func}
+              createdAt={task.createdAt}
+              updatedAt={task.updatedAt}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
