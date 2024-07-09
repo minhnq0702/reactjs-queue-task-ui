0080fd8f2ffe2de7a132e274c4c6758336cded94
MingNe <minhnq.0702@gmail.com>
2024-07-09 10:54:25 +0700

    format

--- src/components/blocks/MessageTable.tsx
+++ src/components/blocks/MessageTable.tsx
@@ -1,7 +1,7 @@
 import { IMessage } from '@/models/TMessage';
 import { getKeyValue, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@nextui-org/react';
 import { useCallback, useMemo } from 'react';
-import { AiOutlineEdit } from 'react-icons/ai';
+// import { AiOutlineEdit } from 'react-icons/ai';
 import { FaCopy, FaEye, FaTrashCan } from 'react-icons/fa6';
 import { toast } from 'sonner';
 import CChipStatus, { STATUS } from '../shared/ChipStatus';
@@ -85,12 +85,12 @@ const CMessageTable = ({ messages, limitCtrl, pageCtrl, onRowClick }: CMessageTa
           >
             <FaEye />
           </span>
-          <span
+          {/* <span
             className="text-lg text-default-400 cursor-pointer active:opacity-50 hover:text-primary-500"
             onClick={() => handleClick(message._id, 'edit')}
           >
             <AiOutlineEdit />
-          </span>
+          </span> */}
           <span
             className="text-medium text-danger-400 cursor-pointer active:opacity-70"
             onClick={() => handleClick(message._id, 'detele')}
