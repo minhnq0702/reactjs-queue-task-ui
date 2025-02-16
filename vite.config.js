0d50d5bc56b25ec0c31da078580ce78e89884ab5
minhnq0702 <minhnq.0702@gmail.com>
2025-02-16 12:05:03 +0700

    feat: vite config for GH page root url

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import react from '@vitejs/plugin-react-swc';
+import * as path from 'path';
+import { defineConfig } from 'vite';
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: '/reactjs-queue-task-ui/',
+    server: {
+        host: '0.0.0.0',
+        port: 8080,
+    },
+    plugins: [react()],
+    resolve: {
+        alias: {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    build: {
+        // * Bundle config
+        cssCodeSplit: true,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    // react: ['react'],
+                    // axios: ['axios'],
+                    // redux: ['redux'],
+                    store: ['@/models/store'],
+                    'react-dom': ['react-dom'],
+                },
+            },
+        },
+    },
+});
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/reactjs-queue-task-ui/',
   server: {
     host: '0.0.0.0',
     port: 8080,
